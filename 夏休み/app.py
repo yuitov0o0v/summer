56681e5a5276eb48305a8709df7929a1ef010664
yuitov0o0v <mishanjiexiang7@gmail.com>
2024-09-01 16:34:33 +0900

    Add files via upload
    夏休み課題

---
+++ 夏休み/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'your_secret_key'
+
+db.init_app(app)
+
+# データベースの初期化
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid credentials"
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return "Welcome to your dashboard!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
